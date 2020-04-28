ebfd9c45d41ac1250742b193c9c9b80919e81774
Andreas Fröhlich <andreas@immerfroehlich.de>
2020-04-28 02:20:21 +0200

    Config: Made the preconfigured archive path to show on generic home path.

---
+++ src/main/java/de/immerfroehlich/javajuicer/model/Configuration.java
@@ -0,0 +1,11 @@
+package de.immerfroehlich.javajuicer.model;
+
+public class Configuration {
+	
+	public String rootPath = "~/Musik/Archiv";
+	
+	//TODO get Drive path from drive selection
+	public String drivePath = "/dev/sr0";
+	
+
+}
