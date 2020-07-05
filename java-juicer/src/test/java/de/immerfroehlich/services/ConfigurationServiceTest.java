fec4318e91af768c7f88f79f6f37451053b9bdc8
Andreas Fröhlich <andreas@immerfroehlich.de>
2020-07-05 17:11:40 +0200

    CommandExecutor didn't work as a subproject as it is also used in other projects. So I included the jar file.

---
+++ java-juicer/src/test/java/de/immerfroehlich/services/ConfigurationServiceTest.java
@@ -0,0 +1,11 @@
+package de.immerfroehlich.services;
+
+public class ConfigurationServiceTest {
+
+	public static void main(String[] args) {
+		ConfigurationService configService = new ConfigurationService();
+		
+		configService.createConfig();
+	}
+
+}
